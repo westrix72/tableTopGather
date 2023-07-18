366fc730f563551183eaa53a17bebee42cd823f2
Lucas <29964166+Lucasnribeiro@users.noreply.github.com>
2023-07-18 10:44:48 -0300

    fixing views and adding a home route

--- tabletopgatherProject/tabletopgatherAPP/views.py
+++ tabletopgatherProject/tabletopgatherAPP/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from tabletopgatherAPP import views
 
 # Create your views here.
+
+
+def index(request):
+    template = loader.get_template("home.html")
+
+    return HttpResponse(template.render())
