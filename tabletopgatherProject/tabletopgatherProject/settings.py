0b439f4716d2ae5e3938ae26b6b34bf5e387e7fe
westrix72 <lfeliperebello@live.com>
2023-07-22 21:05:41 -0300

    changed database to mysql

--- tabletopgatherProject/tabletopgatherProject/settings.py
+++ tabletopgatherProject/tabletopgatherProject/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'tabletopgatherProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tabletopgather',
+        'USER': 'root',
+        'PASSWORD': 'e0PkBN0!2bxp',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
