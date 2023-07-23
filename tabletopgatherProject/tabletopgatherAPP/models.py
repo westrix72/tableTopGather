c0bc696827e989700ebd842d6f25852286044d6d
westrix72 <lfeliperebello@live.com>
2023-07-22 23:42:50 -0300

    Added Address and User models

--- tabletopgatherProject/tabletopgatherAPP/models.py
+++ tabletopgatherProject/tabletopgatherAPP/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.street}, {self.city}, {self.postal_code}"    
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    birthday = models.DateField()
+    email = models.EmailField()
+    cellphone = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
